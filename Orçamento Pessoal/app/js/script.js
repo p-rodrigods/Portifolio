7e97ac3318dfeee610db0c7e748854c34f81f026
p-rodrigods <prds.tj@gmail.com>
2024-03-12 22:04:04 -0300

    Projeto Orçamento Pessoal
    Desenvolvendo conhecimentos em javascript

--- Orçamento Pessoal/app/js/script.js
+++ Orçamento Pessoal/app/js/script.js
@@ -1,6 +1,6 @@
 
-class Despesa{
-    constructor(ano, mes, dia, tipo, descricao, valor){
+class Despesa {
+    constructor(ano, mes, dia, tipo, descricao, valor) {
         this.ano = ano
         this.mes = mes
         this.dia = dia
@@ -8,18 +8,116 @@ class Despesa{
         this.descricao = descricao
         this.valor = valor
     }
+
+    validarDados() {
+        for (let i in this) {
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
+                return false
+            }
+        }
+        return true
+    }
 }
 
-function cadastrarDespesa(){
-  
-   let ano = document.getElementById('ano').value
-   let mes = document.getElementById('mes').value
-   let dia = document.getElementById('dia').value
-   let tipo = document.getElementById('tipo').value
-   let descricao =  document.getElementById('descricao').value
-   let valor =  document.getElementById('valor').value
+class Bd {
+    constructor() {
+        let id = localStorage.getItem('id')
+        if (id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
 
-   let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+    getProximoId() {
+        let proximoId = localStorage.getItem('id')
+        return parseInt(proximoId) + 1
+    }
 
-   console.log(despesa)
+    gravar(d) {
+        let id = this.getProximoId()
+        localStorage.setItem(id, JSON.stringify(d))
+        localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros() {
+        //array de despesas
+        let despesas  = Array()
+
+        let id = localStorage.getItem('id')
+        //Recuperar todas as despesas cadastradas em localStorage
+        for (let i = 1; i < id; i++) {
+            //recuperar a despesa 
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //existe a possibilidade de haver indices removidos
+            //nesses casos vamos pular esse índices
+            if(despesa == null){
+                continue
+            }
+
+            despesas.push(despesa)
+        }
+        return despesas
+    }
+}
+
+let bd = new Bd()
+
+function cadastrarDespesa() {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    if (despesa.validarDados()) {
+        bd.gravar(despesa)
+        document.getElementById('modal_titulo').innerHTML = "Registro inserido com Sucesso"
+        document.getElementById('modal_titulo_div').className = "modal-header text-success"
+        document.getElementById('modal_conteudo').innerHTML = "Despesa cadastrada com sucesso!"
+        document.getElementById('modal_btn').innerHTML = "Voltar"
+        document.getElementById('modal_btn').className = "btn btn-success"
+        $('#modalRegistraDespesa').modal('show')
+    } else {
+        document.getElementById('modal_titulo').innerHTML = "Erro ao inserir o registro"
+        document.getElementById('modal_titulo_div').className = "modal-header text-danger"
+        document.getElementById('modal_conteudo').innerHTML = "Erro na gravação, verifique se todos os campos foram preenchidos corretamente!"
+        document.getElementById('modal_btn').innerHTML = "Voltar e Corrigir"
+        document.getElementById('modal_btn').className = "btn btn-danger"
+        $('#modalRegistraDespesa').modal('show')
+    }
+
+}
+
+function carregaListaDespesas() {
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
+
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    //percorrer o array despeas, listando cada despesa de forma dinâmica
+    despesas.forEach(function(d){
+        //criando a linha (tr)
+        let linha = listaDespesas.insertRow()
+        //criar as colunas (td)
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+        //ajustar o tipo
+        switch(d.tipo){
+            case '1': d.tipo = "Alimentação"
+                break
+            case '2': d.tipo = "Educação"
+                break 
+            case '3': d.tipo = "Lazer"
+                break
+            case '4' : d.tipo = "Saúde"
+                break
+            case '5': d.tipo = "Transporte"
+                break
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+    })
 }
