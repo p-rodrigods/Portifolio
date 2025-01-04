55c6a655ac56f803b6cf8fca022bf56e7a55c186
p.rodrigods <prds.tj@gmail.com>
2025-01-04 15:25:17 -0300

    Atualizações de Lógicas

--- HelpDesk/consultar_chamado.php
+++ HelpDesk/consultar_chamado.php
@@ -9,7 +9,22 @@ while (!feof($arquivo)) {
 
   $registro = fgets($arquivo);
 
-  $chamados[] = $registro;
+  $registro_detalhes = explode('#', $registro);
+
+  if($_SESSION['perfil_id'] == 2 ){
+
+   if($_SESSION['id'] != $registro_detalhes[0]){
+    continue;
+   }else{
+      $chamados[] = $registro;
+   }
+
+  }else{
+     $chamados[] = $registro;
+  }
+
+ 
+ 
 }
 
 fclose($arquivo);
@@ -61,17 +76,9 @@ fclose($arquivo);
             <?php foreach ($chamados as $chamado) { ?>
               <?php 
                 $chamado_dados = explode('#', $chamado);
-                
-                if($_SESSION['perfil_id'] == 2 ){
-                  if($_SESSION['id'] != $chamado_dados[0]){
-                    continue;
-                  }
-                }
-
-                if(count($chamado_dados)<3){
+                if(count($chamado_dados) < 3) {
                   continue;
                 }
-                
               ?>
               <div class="card mb-3 bg-light">
                 <div class="card-body">
